1a42e51ae472dadafaece6219d51f66beb6c0ae3
Leoltron <leoole5@gmail.com>
2019-10-17 23:26:40 +0500

    Initial commit

---
+++ src/main/kotlin/ru/leoltron/onmeeting/OnMeetingApplication.kt
@@ -0,0 +1,11 @@
+package ru.leoltron.onmeeting
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OnMeetingApplication
+
+fun main(args: Array<String>) {
+    runApplication<OnMeetingApplication>(*args)
+}
