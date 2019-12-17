1e5c8c11f997d7051857d51dcc5fd80e8502eb7c
jellymary <marykuzovleva@gmail.com>
2019-12-17 23:22:56 +0500

    fix eslint errors

--- src/main/front/src/components/Board/index.tsx
+++ src/main/front/src/components/Board/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import {useHistory} from "react-router-dom";
-import {getPrincipal} from "../../httpClient";
-import report from "../../utils/report";
+// import {useHistory} from "react-router-dom";
+// import {getPrincipal} from "../../httpClient";
+// import report from "../../utils/report";
 
 const Board: React.FC = () => {
-    const history = useHistory();
+    // const history = useHistory();
     // getPrincipal()
     //     .then(p => {
     //         if (!p.authorized) history.push("/")
