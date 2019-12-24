c40d4efc25e580219fe2287645a0d88ce710b0ab
Leoltron <leoole5@gmail.com>
2019-12-25 03:55:29 +0500

    Added app download icon

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # OnMeeting
 
-[![Build Status](https://travis-ci.com/Leoltron/OnMeeting.svg?branch=master)](https://travis-ci.com/Leoltron/OnMeeting)
+[Android app](https://github.com/Leoltron/OnMeetingAndroidClient)
--- src/main/front/src/components/Header/index.tsx
+++ src/main/front/src/components/Header/index.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import "./Header.less";
+import android from './android-48px.svg';
 
 const Header: React.FC = () => (
     <div className="header">
+        <a href="/on-meeting.apk" className="download-app-link"><img src={android}/></a>
         <h1>OnMeeting</h1>
     </div>
 );
